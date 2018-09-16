56677f36dddad0f83511e50d4fc4274198fa1e6f
linhuaxin <linhuaxin@foxmail.com>
2018-09-16 11:12:53 +0800

    泛型

--- src/com/linhuaxin/algorithm/SelectionSort.java
+++ src/com/linhuaxin/algorithm/SelectionSort.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class SelectionSort {
 
-    public static void sort(int[] arr, int n) {
+    public static <T extends Comparable<T>> void sort(T[] arr, int n) {
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if (arr[minIndex] > arr[j]) {
+                if (arr[j].compareTo(arr[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
             if (minIndex != i) {
-                int temp = arr[i];
+                T temp = arr[i];
                 arr[i] = arr[minIndex];
                 arr[minIndex] = temp;
             }
@@ -21,7 +21,7 @@ public class SelectionSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 3, 5, 9, 7};
+        Integer arr[] = {1, 3, 5, 9, 7};
         sort(arr, arr.length);
         System.out.println(Arrays.toString(arr));
     }
